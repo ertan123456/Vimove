99cdc37b9ad5433b48cf95e20ad3a70a90c8e925
ertan123456 <erdemertan08@gmail.com>
2025-10-06 20:24:58 +0300

    Initial commit - ViMove full project

--- app/static/contents.js
+++
@@ -1,8 +0,0 @@
-// game.py'deki içerik/sabitlerin bire bir karşılığı:
-window.VIMOVE_CONTENT = {
-  TARGET_DISTANCE: 60,
-  HOLD_MS: 800,
-  LEVELS: [{speed:1.0},{speed:1.2},{speed:1.5}],
-  COLORS: { main: "#7aa2f7" },
-  TEXTS: { start: "Oyunu Başlat", stop: "Durdur", score: "Skor" }
-};
